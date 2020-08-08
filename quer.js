ffb9e52a1ed872b73ba783b615fe3da9ca371b19
Ogunmokun Ademola <ademolaogunmokun@yahoo.com>
2020-08-08 10:27:55 +0100

    my first try

--- quer.js
+++ quer.js
@@ -5,7 +5,7 @@ contactIdx = contactIdx.replace('For any enquiries you can','Enquiries?');
 contactId.firstChild.nodeValue = contactIdx; 
 $(document).ready(function() {
     $("h1").click(function(){
-        alert("Thanks for visiting the website");});
+         alert("Thanks for visiting the website");});
     $("details").css("background-color","white");
     $("li:first").width(100);
 });
