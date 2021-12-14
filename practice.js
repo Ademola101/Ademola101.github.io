53d99b693fb53b5c77a7fd04c577b3c456a35768
Ademola <ademolaogunmokun@yahoo.com>
2021-12-14 20:14:59 +0100

    delete redundant files

--- practice.js
+++ practice.js
@@ -4,3 +4,35 @@ function myFullName(first,second,third){
     
 };
 myFullName("Ademola","Israel","Ogunmokun");
+
+
+function sayHello(){
+    message = "Hello ademola";
+    // remove var it becomes global
+};
+console.log(message);
+sayHello()
+console.log(message);
+
+function myAge() {
+    age = 20
+    console.log(age);
+    // returned undefined
+    var age = 15
+}
+
+myAge()
+
+if(true){
+    var name = "matt"
+    console.log(name);
+}
+
+console.log(name);
+
+if(true){
+    let name = "matt"
+    console.log(name);
+}
+// this trhow error
+console.log(name);
